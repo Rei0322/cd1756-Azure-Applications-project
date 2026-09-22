932f09ca95fe3372aff8171c27289dd68c82cca3
Samira N Ray <ndea0322@gmail.com>
2026-09-21 21:13:42 -0400

    Configure logging for Flask application
    Added logging configuration to send INFO-and-above messages to a stream handler.

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,18 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# Logging: send INFO-and-above messages (including login attempts) to a
+# stream handler. On Azure App Service this shows up in the Log stream
+# under Monitoring > App Service logs, once application logging is turned on.
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s in %(module)s: %(message)s'
+))
+app.logger.addHandler(stream_handler)
+app.logger.setLevel(logging.INFO)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
